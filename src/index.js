b4f8b9b45fb75d137d277a679d62d9128b44228a
Anna Samsonenko <foster.anna93@gmail.com>
2021-11-10 20:43:03 +0200

    add initial files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-import './sass/main.scss';
+import './css/styles.css';
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix';
+import countriesListTemplate from './templates/countries-list.hbs';
+import countryCardTemplate from './templates/country-card.hbs';
+import CountriesApiService from './js/countries-api-service';
