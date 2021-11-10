84199c075b642edbd9841d95ac239cbf387368a0
Anna Samsonenko <foster.anna93@gmail.com>
2021-11-11 00:16:40 +0200

    that was bullshit 🤬🤬🤬🤬🥵😡🤬

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 import './sass/main.scss';
-import '../node_modules/modern-normalize/modern-normalize.css';
 import { Notify } from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-import * as bootstrap from 'bootstrap';
