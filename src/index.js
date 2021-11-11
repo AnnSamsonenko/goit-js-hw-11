3117f49909adb0f62331f2d2b239a9518015c96c
Anna Samsonenko <foster.anna93@gmail.com>
2021-11-11 16:33:11 +0200

    add some solutions

--- src/index.js
+++ src/index.js
@@ -2,3 +2,56 @@ import './sass/main.scss';
 import { Notify } from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import GalleryApiService from './js/gallery-api-service';
+import cardsTemplate from './templates/cards-grid.hbs';
+import LoadMoreBtn from './js/components/load-more-btn';
+
+const refs = {
+  searchForm: document.querySelector('#search-form'),
+  cardsContainer: document.querySelector('.gallery'),
+};
+
+console.log(refs.cardsContainer);
+
+const loadMoreBtn = new LoadMoreBtn({
+  selector: '[data-action="load-more"]',
+  hidden: true,
+});
+
+const galleryApiService = new GalleryApiService();
+
+refs.searchForm.addEventListener('input', onSearch);
+loadMoreBtn.refs.button.addEventListener('click', fetchMarkup);
+
+function onSearch(e) {
+  e.preventDefault();
+
+  galleryApiService.query = e.target.value;
+
+  if (galleryApiService.query === '') {
+    Notify.failure('------');
+    return;
+  }
+
+  loadMoreBtn.show();
+  galleryApiService.resetPage();
+  clearCardsContainer();
+  fetchMarkup();
+}
+
+function fetchMarkup() {
+  loadMoreBtn.disable();
+  galleryApiService.fetchCards().then(cards => {
+    console.log(cards);
+    appendCardsMarkup(cards);
+    loadMoreBtn.enable();
+  });
+}
+
+function appendCardsMarkup(cards) {
+  refs.cardsContainer.insertAdjacentHTML('beforeend', cardsTemplate({ ...cards.hits }));
+}
+
+function clearCardsContainer() {
+  refs.cardsContainer.innerHTML = '';
+}
--- src/js/gallery-api-service.js
+++ src/js/gallery-api-service.js
@@ -1,1 +1,34 @@
+const API_KEY = 'key=23901512-731f3652ce829a0e5db3ff14f';
+const BASE_URL = 'https://pixabay.com/api/';
 
+export default class GalleryApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+
+  fetchCards() {
+    const url = `${BASE_URL}?${API_KEY}&q=${this.searchQuery}&image_type=photo&per_page=40&page=${this.page}&orientation=horizontal&safesearch=true`;
+
+    return fetch(url).then(response => {
+      this.incrementPage();
+      return response.json();
+    });
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
